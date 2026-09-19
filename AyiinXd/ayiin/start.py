4da59e84b6fa2cab11a211a3e07bf0a83679caba
customcust <x888777111@gmail.com>
2026-09-19 13:56:06 +0700

    Update image and caption in start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -20,10 +20,10 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/b17248930f58bbdaf2e23.jpg",
-                caption="𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
-                buttons=[(Button.url("Store", "https://t.me/jasebxall")),
-                         (Button.url("Support", "https://t.me/ixallsupport"))]
+                "https://graph.org/file/fe8d9e0c144319ad0d279-cb30050a378e6b5801.jpg",
+                caption="𝗞𝗔𝗪𝗔𝗜𝗜-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                buttons=[(Button.url("LPM", "https://t.me/lpm_jualan_kebsos")),
+                         (Button.url("STORE", "https://t.me/choumist"))]
             )
     except Exception as e:
         LOGS.error(e)
